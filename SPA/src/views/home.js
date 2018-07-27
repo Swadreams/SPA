2f592096f73d4ce0aabd389a637c29dac26494c9
Swapnil Chute <schute@mobiquityinc.com>
2018-07-27 17:37:25 +0530

    27/07/2018: Added Navigation Package.

---
+++ SPA/src/views/home.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import {
+    StyleSheet,
+    Text,
+    View
+} from 'react-native';
+
+export default class Home extends Component {
+    static navigationOptions = {
+        header: null
+    }
+
+    render() {
+        const {navigate} = this.props.navigation;
+
+        return (
+            <View style={styles.container}>                
+                <Text> This is Home Component </Text>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    }
+})
