a1ceec79126e8713403a4f9fc1c4a1779f5d300e
Swapnil Chute <schute@mobiquityinc.com>
2018-07-30 12:17:05 +0530

    30/07/2018: Integrated Fonts, Resolved Errors

--- SPA/App.js
+++ SPA/App.js
@@ -6,11 +6,33 @@ import Home from './src/views/home';
 
 const Routes = createStackNavigator({
   Home: { 
-    screen: Home 
+    screen: Home,
   },  
 });
 export default class App extends React.Component {
+  // static navigatorOptions = {
+  //       header: null
+  //   }
+  constructor() {
+    super();
+    this.state = {
+      readyFlag: false,
+    };
+  }
+  
+  async componentWillMount() {
+    await Expo.Font.loadAsync({
+      'Roboto': require('./Fonts/Roboto.ttf'),
+      'Roboto_medium': require('./Fonts/Roboto_medium.ttf'),        
+      'Ionicons': require('@expo/vector-icons/fonts/Ionicons.ttf'),
+    });
+    this.setState({ readyFlag: true });
+  }
+
   render() {
+    if (!this.state.readyFlag) {
+      return <Expo.AppLoading />;
+    }
     return (
       <Routes />
     );
--- SPA/src/views/home.js
+++ SPA/src/views/home.js
@@ -1,28 +1,27 @@
 import React, { Component } from 'react';
-import {
-    StyleSheet,
-    Text,
-    View
-} from 'react-native';
-
-export default class Home extends Component {
-    static navigationOptions = {
-        title: 'Home'
-    }
-
-    render() {
-        const {navigate} = this.props.navigation;
-
-        return (
-            <View style={styles.container}>                
-                <Text> This is Home Component </Text>
-            </View>
-        )
-    }
+import { StyleSheet } from 'react-native';
+import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Text } from 'native-base';
+export default class AnatomyExample extends Component {
+  static navigationOptions = {
+    title: 'Welcome',
+  };
+  render() {
+    return (
+      <Container style={styles.container}>
+          <Text> Text test </Text>
+      </Container>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1
-    }
+  container: {
+    flex: 1,
+    backgroundColor: 'green'
+  },
+  headerText: {
+    textAlign: 'right',
+    color: '#fff',
+    fontSize: 20
+},
 })
